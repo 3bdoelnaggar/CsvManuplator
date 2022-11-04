6a9ede086aba1ccea025d1a266823060c5d9b02e
mahmoud-elnaggar <3bdoelnaggar@gmail.com>
2022-11-04 16:53:13 +0200

    WIP

--- src/main/kotlin/CSVModel.java
+++
@@ -1,9 +0,0 @@
-import java.lang.String;
-
-public class CSVModel {
-  public String name = "Abdalla";
-
-  public String address = "Alexandria";
-
-  public String email = "3bdoelnaggar@gmail.com";
-}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -8,11 +9,11 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import com.squareup.javapoet.FieldSpec
-import com.squareup.javapoet.JavaFile
 import com.squareup.javapoet.TypeSpec
 import java.io.*
 
@@ -22,8 +23,8 @@ fun App() {
     val file = File("src//main//resources//input.csv")
 
 
-    val keys :Array<String>
-    val values:Array<String>
+    val keys: Array<String>
+    val values: Array<String>
     val builder: TypeSpec.Builder = TypeSpec.classBuilder("CSVModel")
         .addModifiers(javax.lang.model.element.Modifier.PUBLIC)
 
@@ -33,52 +34,69 @@ fun App() {
         keys = firstLine.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
         values = bufferedReader.readLine().split(",".toRegex()).dropLastWhile { it.isEmpty() }
             .toTypedArray()
-        for (i in keys.indices) {
-            var value = ""
-            if (i < values.size) {
-                value = values[i]
-            }
-            val field: FieldSpec = FieldSpec.builder(String::class.java, keys[i])
-                .addModifiers(javax.lang.model.element.Modifier.PUBLIC)
-                .initializer("\$S", value)
-                .build()
-            builder.addField(field)
-        }
-        val typeSpec: TypeSpec = builder.build()
-        val javaFile: JavaFile = JavaFile.builder("", typeSpec).build()
-        javaFile.writeTo(File("src//main//kotlin/"))
+//        for (i in keys.indices) {
+//            var value = ""
+//            if (i < values.size) {
+//                value = values[i]
+//            }
+//
+//        }
+//            val field: FieldSpec = FieldSpec.builder(String::class.java, keys[i])
+//                .addModifiers(javax.lang.model.element.Modifier.PUBLIC)
+//                .initializer("\$S", value)
+//                .build()
+//            builder.addField(field)
+//        }
+//        val typeSpec: TypeSpec = builder.build()
+//        val javaFile: JavaFile = JavaFile.builder("", typeSpec).build()
+//        javaFile.writeTo(File("src//main//kotlin/"))
 //        generate()
     } catch (e: IOException) {
         throw RuntimeException(e)
     }
 
 
-    val map = LinkedHashMap<String,String>()
+    val map = LinkedHashMap<String, String>()
 
 
     MaterialTheme {
-        Column {
-            for(i in keys.indices){
-                Text(keys[i])
-                val textValue = remember {
-                    map[keys[i]] = values[i]
+        val stateVertical = rememberScrollState(0)
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(stateVertical)
+                .padding(end = 12.dp, bottom = 12.dp)
+        ) {
+            Column {
+                for (i in keys.indices) {
+                    Text(keys[i])
+                    val textValue = remember {
+                        map[keys[i]] = values[i]
 
-                    mutableStateOf(values[i])
+                        mutableStateOf(values[i])
 
+                    }
+                    TextField(modifier = Modifier, value = textValue.value, onValueChange = { input ->
+                        textValue.value = input
+                        map[keys[i]] = input
+                    })
                 }
-                TextField(modifier = Modifier, value = textValue.value, onValueChange = {input ->
-                    textValue.value = input
-                    map[keys[i]] = input
-                })
+                Button(onClick = {
+                    generateFile(map)
+                }) {
+                    Text("Generate")
+                }
             }
-            Button(onClick = {
-                generateFile(map)
-            }){
-                Text("Generate")
-            }
+            HorizontalScrollbar(
+                modifier = Modifier.align(Alignment.CenterEnd)
+                    .fillMaxHeight(),
+                adapter = rememberScrollbarAdapter(stateVertical)
+            )
         }
     }
+
 }
+
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
@@ -117,48 +135,48 @@ private fun generateFile(map: LinkedHashMap<String, String>) {
 }
 
 
-private fun generate() {
-    val file = File("src//main//resources//output.csv")
-    var fileWriter: FileWriter? = null
-    fileWriter = try {
-        FileWriter(file)
-    } catch (e: IOException) {
-        throw RuntimeException(e)
-    }
-    val header = StringBuilder()
-    val aClass: Class<CSVModel> = CSVModel::class.java
-    for (field in aClass.getFields()) {
-        header.append(field.name)
-        header.append(",")
-    }
-    header.deleteCharAt(header.length - 1)
-    try {
-        fileWriter?.write(header.toString())
-        fileWriter?.write("\n")
-    } catch (e: IOException) {
-        throw RuntimeException(e)
-    }
-    val csvModel = CSVModel()
-    val line = StringBuilder()
-    for (field in aClass.getFields()) {
-        try {
-//            if (field.name == paramter.key()) {
-//                line.append(paramter.value())
-//            } else {
-                line.append(field[csvModel])
-//            }
-            line.append(",")
-        } catch (e: IllegalAccessException) {
-            throw RuntimeException(e)
-        }
-    }
-    line.deleteCharAt(line.length - 1)
-    try {
-        fileWriter?.write(line.toString())
-        fileWriter?.write("\n")
-        fileWriter?.close()
-    } catch (e: IOException) {
-        throw RuntimeException(e)
-    }
-}
+//private fun generate() {
+//    val file = File("src//main//resources//output.csv")
+//    var fileWriter: FileWriter? = null
+//    fileWriter = try {
+//        FileWriter(file)
+//    } catch (e: IOException) {
+//        throw RuntimeException(e)
+//    }
+//    val header = StringBuilder()
+//    val aClass: Class<CSVModel> = CSVModel::class.java
+//    for (field in aClass.getFields()) {
+//        header.append(field.name)
+//        header.append(",")
+//    }
+//    header.deleteCharAt(header.length - 1)
+//    try {
+//        fileWriter?.write(header.toString())
+//        fileWriter?.write("\n")
+//    } catch (e: IOException) {
+//        throw RuntimeException(e)
+//    }
+//    val csvModel = CSVModel()
+//    val line = StringBuilder()
+//    for (field in aClass.getFields()) {
+//        try {
+////            if (field.name == paramter.key()) {
+////                line.append(paramter.value())
+////            } else {
+//                line.append(field[csvModel])
+////            }
+//            line.append(",")
+//        } catch (e: IllegalAccessException) {
+//            throw RuntimeException(e)
+//        }
+//    }
+//    line.deleteCharAt(line.length - 1)
+//    try {
+//        fileWriter?.write(line.toString())
+//        fileWriter?.write("\n")
+//        fileWriter?.close()
+//    } catch (e: IOException) {
+//        throw RuntimeException(e)
+//    }
+//}
 
